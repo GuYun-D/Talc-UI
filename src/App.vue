57215ebec820719cda5750e8d9589aad3caa2b14
GuYun-D <1946306647@qq.com>
2021-07-28 16:49:39 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>你好轮子</div>
+  <First></First>
+</template>
+
+<script>
+import First from './components/First.vue'
+
+export default {
+  name: 'App',
+  components: {
+    First
+  }
+}
+</script>
