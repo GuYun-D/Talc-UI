b16c066d7775b5a71ca0acdaa44d36e5a434a2f8
GuYun-D <1946306647@qq.com>
2021-07-31 11:08:42 +0800

    关于v-model的一些测试

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <Button>你好</Button>
+    <!-- 
+      默认会将绑定的属性都绑定到根元素
+      Button ：
+        <div  @mouseover="onClick" @focus="onClick"  @click="onClick">
+          <button></button>
+        </div>
+     -->
+    <Button @mouseover="onClick" @focus="onClick"  @click="onClick">你好</Button>
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
-  export default {
-    components: {
-      Button
-    }
-  }
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+
+  setup() {
+    const onClick = () => {
+      console.log("执行了");
+    };
+
+    return {onClick}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,31 @@
 <template>
-  <button>
-    <slot />
-  </button>
+  <div :size="size">
+    <!-- 
+      将所有属性全部绑定到button属性上
+      <button v-bind="$attr">
+        <slot />
+      </button>
+    -->
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Button"
-  }
+export default {
+  name: "Button",
+  // 取消属性继承
+  inheritAttrs: false,
+
+  // 属性分两部分
+  setup(props, context) {
+    const { size, ...rest } = context.$attrs;
+
+    return { size, rest };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
