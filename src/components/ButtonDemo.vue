1c538d990a39429554d2a51864b9a3753b8d4325
GuYun-D <1946306647@qq.com>
2021-07-31 13:43:30 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,6 +5,28 @@
     <Button theme="button">就是按钮</Button>
     <Button theme="link">链接形式</Button>
     <Button theme="text">文本形式</Button>
+
+    <h3>案例二</h3>
+    <Button>normal</Button>
+    <Button size="big">big</Button>
+    <Button size="small">small</Button>
+
+    <h3>案例三</h3>
+    <div>
+      <Button size="big" level="main">主要按钮</Button>
+      <Button size="normal">普通按钮</Button>
+      <Button size="small" level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button size="big" theme="link" level="main">主要按钮</Button>
+      <Button size="normal" theme="link">普通按钮</Button>
+      <Button size="small" theme="link" level="danger">危险按钮</Button>
+    </div>
+     <div>
+      <Button size="big" theme="text" level="main">主要按钮</Button>
+      <Button size="normal" theme="text">普通按钮</Button>
+      <Button size="small" theme="text" level="danger">危险按钮</Button>
+    </div>
   </div>
 </template>
 
@@ -20,13 +42,14 @@ export default {
       console.log("执行了");
     };
 
-    return {onClick}
+    return { onClick };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-h3{
-  padding-bottom: 20px;
+h3 {
+  color: #42b983;
+  margin: 20px 0;
 }
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,12 @@
 <template>
-  <button class="t-button" :class="{[`t-theme-${theme}`]: theme}">
+  <button class="t-button" :class="classes">
     <slot />
   </button>
 </template>
 
 <script>
+import { computed } from "vue";
+
 export default {
   name: "Button",
   props: {
@@ -17,7 +19,25 @@ export default {
       type: String,
       default: "normal",
     },
-  }
+
+    level: {
+      type: String,
+      default: "normal",
+    },
+  },
+
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`t-theme-${theme}`]: theme,
+        [`t-size-${size}`]: size,
+        [`t-level-${level}`]: level,
+      };
+    });
+
+    return { classes };
+  },
 };
 </script>
 
@@ -27,6 +47,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .t-button {
   box-sizing: border-box;
   height: $h;
@@ -41,6 +62,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -55,20 +77,81 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-   &.t-theme-link{
+  &.t-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.t-theme-text{
+  &.t-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.t-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.t-size-small {
+    font-size: 12px;
+    height: 28px;
+    padding: 0 4px;
+  }
+
+  &.t-theme-button {
+    &.t-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.t-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.t-theme-link {
+    &.t-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.t-theme-text {
+    &.t-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.t-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
 }
