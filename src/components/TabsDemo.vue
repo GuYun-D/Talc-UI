6c46092d0d89a229614ba10392cbf4705302602d
GuYun-D <1946306647@qq.com>
2021-08-03 09:30:23 +0800

    检查1子组件的类型

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <Tabs>
-      <Table title="导航一">内容一</Table>
-      <Table title="导航二">内容二</Table>
+      <Tab title="导航一">内容一</Tab>
+      <Tab title="导航二">内容二</Tab>
     </Tabs>
   </div>
 </template>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Tabs组件</div>
+  <div>
+    <component :is="defaults[0]" />
+    <component :is="defaults[1]" />
+  </div>
 </template>
 
 <script lang="ts">
+import Tab from "./Tab.vue";
 export default {
-  name: "Tabs"
+  name: "Tabs",
+  setup(props, context) {
+    /**
+     * defaults就是关于子组件的信息 类型是一个数组，每个元素就是Tabs子组件的信息
+     * 利用这个信息可以对子组件的类型进行判断，规定用户使用1正确的组件
+     * 每一条信息 === 子组件Tab
+     */
+    const defaults = context.slots.default();
+
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs子组件必须是Tab");
+      }
+    });
+    return { defaults };
+  },
 };
 </script>
 
