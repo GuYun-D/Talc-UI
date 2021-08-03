7a6fdbcef6d609918a49f0c263c8432b0a8e7f63
GuYun-D <1946306647@qq.com>
2021-08-03 09:09:41 +0800

    创建Tabs和Tab组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>tab文档</div>
+  <div>
+    <Tabs>
+      <Table title="导航一">内容一</Table>
+      <Table title="导航二">内容二</Table>
+    </Tabs>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/lib/Tab.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Tab组件</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tab",
+};
+</script>
+
+<style lang="scss">
+</style>
