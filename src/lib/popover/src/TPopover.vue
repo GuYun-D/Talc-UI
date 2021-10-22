ce7b51692c3a1e334ef2d5cc98af7d6e864c556b
GuYun-D <1946306647@qq.com>
2021-10-22 14:50:56 +0800

    添加样式

--- src/lib/popover/src/TPopover.vue
+++ src/lib/popover/src/TPopover.vue
@@ -21,6 +21,7 @@ import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "t-popover",
+  // 可优化的
   setup() {
     const popoverVisible = ref(false);
     const triggerRef = ref(null);
@@ -29,6 +30,11 @@ export default defineComponent({
 
     const popoverClick = () => {
       popoverVisible.value = !popoverVisible.value;
+      document.removeEventListener("click", popoverClose);
+      function popoverClose() {
+        popoverVisible.value = false;
+        document.removeEventListener("click", popoverClose);
+      }
 
       if (popoverTimer) {
         clearTimeout(popoverTimer);
@@ -38,13 +44,7 @@ export default defineComponent({
           const { top, left } = triggerRef.value.getBoundingClientRect();
           contentRef.value.style.top = top + window.screenY + "px";
           contentRef.value.style.left = left + window.screenX + "px";
-          function popoverClose() {
-            popoverVisible.value = false;
-            document.removeEventListener("click", popoverClose);
-          }
           document.addEventListener("click", popoverClose);
-        } else {
-          console.log("关闭popover");
         }
       });
     };
@@ -62,9 +62,36 @@ export default defineComponent({
 
 .content-wrapper {
   position: absolute;
-  border: 1px solid #f40;
-  box-shadow: 1px 1px 1px #ccc;
+  border: 1px solid rgb(168, 168, 168);
+  // box-shadow: 1px 1px 1px rgb(189, 189, 189);
   transform: translateY(-100%);
+  border-radius: 5px;
+  margin-top: -10px;
+  padding: 0.5em 1em;
   z-index: 10;
+  filter: drop-shadow(0px 1px 1px rgb(189, 189, 189));
+  background-color: #fff;
+  word-break: break-all;
+
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    border: 10px solid transparent;
+    width: 0px;
+    height: 10px;
+    position: absolute;
+    left: 10px;
+  }
+
+  &::before {
+    border-top-color: rgb(168, 168, 168);
+    top: 100%;
+  }
+
+  &::after {
+    top: calc(100% - 1px);
+    border-top-color: rgb(255, 255, 255);
+  }
 }
 </style>
