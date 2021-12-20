df2d6786f44da62c2f883af115c356d2ba1da74d
GuYun-D <1946306647@qq.com>
2021-12-20 20:50:59 +0800

    边边角角

---
+++ src/lib/footer/src/TFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="t-footer">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 't-footer',
+  setup() {
+    return {};
+  },
+});
+</script>
---
+++ src/lib/header/src/THeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="t-header">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 't-header',
+  setup() {
+    return {};
+  },
+});
+</script>
